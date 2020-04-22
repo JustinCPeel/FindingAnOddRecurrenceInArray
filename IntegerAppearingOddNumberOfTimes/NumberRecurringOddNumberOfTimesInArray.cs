d70169600d1aed9853d5c64ab563c6a49177463e
Justin Peel <justinpeel25@gmail.com>
2020-04-22 10:24:08 +0200

    Initial Commit

---
+++ IntegerAppearingOddNumberOfTimes/NumberRecurringOddNumberOfTimesInArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace IntegerAppearingOddNumberOfTimes
+{
+	[TestFixture]
+	public class NumberAppearingOddNumberOfTimes
+	{
+		[Test]
+		public void GivenNumbers_ThatHasNumbersRecurringOddNumberOfTimes_ShouldReturnNumber()
+		{
+			//arrange
+			var numbers = new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 };
+			//act
+			var expected = 5;
+			var actual = EvenOrOddRecurrenceOfNumber(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		private int EvenOrOddRecurrenceOfNumber(int[] numbers)
+		{
+			int count = 0;
+			foreach(var num in numbers)
+			{
+				for (int i = 0; i < numbers.Length; i++)
+				{
+					if (num == numbers[i])
+						count++;
+				}
+				if (count % 2 != 0)
+					return num;
+			}
+			return 0;
+		}
+	}
+}
---
+++ IntegerAppearingOddNumberOfTimes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegerAppearingOddNumberOfTimes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegerAppearingOddNumberOfTimes")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9d7b78d-90c1-4090-b41d-55587901e481")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
